39ff1c4ca2a992a574d390dd0a1faadc6c754d5b
dvpsky <haneulgim95@gmail.com>
2026-06-29 20:20:39 +0900

    AdminNoticeUpdatePage 작성 완료

--- api/admin/adminNoticeApi.ts
+++ api/admin/adminNoticeApi.ts
@@ -7,8 +7,13 @@ const createNotice = async (input: AdminNoticeInputType): Promise<Notice> => {
     return response.data.data;
 };
 
+const updateNotice = async (id: number, input: AdminNoticeInputType): Promise<Notice> => {
+    const response = await axiosInstance.patch(`/admin/notice/${id}`, input);
+    return response.data.data;
+};
+
 const deleteNotice = async (id: number): Promise<void> => {
     await axiosInstance.delete(`/admin/notice/${id}`);
 };
 
-export default { createNotice, deleteNotice };
+export default { createNotice, updateNotice, deleteNotice };
