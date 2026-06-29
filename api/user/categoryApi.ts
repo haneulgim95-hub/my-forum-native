cb56dae86e135f479e909c6534a18800612dfa46
dvpsky <haneulgim95@gmail.com>
2026-06-29 21:26:52 +0900

    MainHeaderMobile.tsx 작성 완료

---
+++ api/user/categoryApi.ts
@@ -0,0 +1,9 @@
+import axiosInstance from "@/api/axiosInstance";
+import { Category } from "@/types/category";
+
+const getCategoryList = async (): Promise<Category[]> => {
+    const response = await axiosInstance.get("/category");
+    return response.data.data;
+};
+
+export default { getCategoryList };
