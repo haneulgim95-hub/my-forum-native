433993f58827b4dc5c1abc6e6fa5bee658a5ffef
dvpsky <haneulgim95@gmail.com>
2026-06-30 20:16:14 +0900

    카테고리 목록 불러오는 useEffect를 layout으로 옮기기

--- components/layouts/main/MainHeaderDesktop.tsx
+++ components/layouts/main/MainHeaderDesktop.tsx
@@ -1,7 +1,19 @@
-import TextComponent from "@/components/common/text/TextComponent";
+import { useRouter } from "expo-router";
+import { useThemeStore } from "@/stores/theme/useThemeStore";
+import { useAuthStore } from "@/stores/auth/useAuthStore";
+import { View } from "react-native";
+import { Category } from "@/types/category";
 
-function MainHeaderDesktop() {
-    return <TextComponent>desktop</TextComponent>
+interface Props {
+    list: Category[];
+}
+
+function MainHeaderDesktop({ list }: Props) {
+    const router = useRouter();
+    const { theme, onChangeTheme } = useThemeStore();
+    const { isLoggedIn, user, logout } = useAuthStore();
+
+    return <View className={"w-full text-center"}>{list ? "불러와짐" : "실패함"}</View>;
 }
 
 export default MainHeaderDesktop;
--- components/layouts/main/MainHeaderMobile.tsx
+++ components/layouts/main/MainHeaderMobile.tsx
@@ -1,9 +1,8 @@
 import { useRouter, Link } from "expo-router";
 import { useThemeStore } from "@/stores/theme/useThemeStore";
 import { useAuthStore } from "@/stores/auth/useAuthStore";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Category } from "@/types/category";
-import categoryApi from "@/api/user/categoryApi";
 import { twMerge } from "tailwind-merge";
 import TextComponent from "@/components/common/text/TextComponent";
 import { Feather, Ionicons } from "@expo/vector-icons";
@@ -14,25 +13,17 @@ import { Role } from "@/types/user";
 import Button from "@/components/common/button/Button";
 import Accordion from "@/components/common/accordion/Accordion";
 
-function MainHeaderMobile() {
+interface Props {
+    list: Category[];
+}
+
+function MainHeaderMobile({list}:Props) {
     const router = useRouter();
     const { theme, onChangeTheme } = useThemeStore();
     const { isLoggedIn, user, logout } = useAuthStore();
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [list, setList] = useState<Category[]>([]);
 
-    useEffect(() => {
-        const loadCategories = async () => {
-            try {
-                const result = await categoryApi.getCategoryList();
-                setList(result);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        loadCategories().then(() => {});
-    }, []);
 
     const handleNavigate = (path: string) => {
         setIsMenuOpen(false);
